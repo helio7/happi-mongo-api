1726d2fce590199ea7208c444ae39088877da291
Dylan Gimenez <dylan.gimenez.nahuel@gmail.com>
2022-10-15 13:50:57 -0300

    feat: added connection to MongoDB database

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const Hapi = require('@hapi/hapi');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const init = async () => {
+   const {
+      MONGODB_USERNAME,
+      MONGODB_PASSWORD,
+      MONGODB_CLUSTER_HOST,
+   } = process.env;
+
+   const client = new MongoClient(
+      `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_CLUSTER_HOST}/?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }
+   );
+
+   await client.connect()
+      .catch((err) => {
+         console.log(`Couldn't connect to the MongoDB database.`);
+         console.log(err);
+      });
+
    const server = Hapi.server({
       port: 3000,
       host: 'localhost',
